fa4783ced845f608570a0006ec58f9367a7822c1
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-07-18 20:54:02 +0000

    Defined function validate_data.

--- run.py
+++ run.py
@@ -32,3 +32,27 @@ ask_for_print_qestions(data)
 
 print("You")
 
+def get_sales_data():
+    """
+    Gert sales figures from user.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, seperated by commas.")
+    print("Example: 10, 20 30")
+
+    data_str = input("Enter your data here: ")
+
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+ 
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    print(values)
+
+validate_data(data)
