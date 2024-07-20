66f1c34a0d7b0f0c0b727cd7e6aa4d80cf3667e3
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-07-20 17:52:29 +0000

    Add function update_sales_worksheet().

--- run.py
+++ run.py
@@ -15,36 +15,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
-
-data = sales.get_all_values()
-
-
-print(data)
-
-def ask_for_print_qestions(data):
-    """
-    starts a quiz by asking question
-    """
-    print(f"co chcesz zrobic {data}")
-
-ask_for_print_qestions(data)
-
-print("You")
-
 def get_sales_data():
     """
-    Gert sales figures from user.
+    Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10, 20 30")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
- 
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
 
 
 def validate_data(values):
@@ -57,9 +45,28 @@ def validate_data(values):
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
-                f"Exactly 6 values required ,you provided {len(values)}"
-            ) 
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again \n.")
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-get_sales_data()
+    return True
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated succesfully")
+
+
+
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
+
