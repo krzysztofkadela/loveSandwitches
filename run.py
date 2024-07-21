1c874099305c01e3f3934fe0534f7a3127d9617c
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-07-21 16:58:29 +0000

    Function for updateoing workshhets in spreadsheet added.

--- run.py
+++ run.py
@@ -96,7 +96,15 @@ def update_surplus_worksheet(surplus_data):
     surplus_worksheet.append_row(surplus_data)
     print("Surplus worksheet updated successfully.\n")
 
-
+def update_worksheet(data, worksheet):
+    """
+    Recivers a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(F"Updating {worksheet} worksheet...")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated./n")
 
 
 
@@ -106,14 +114,16 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
+    update_worksheet(sales_data, "sales")
+    """
     update_sales_worksheet(sales_data)
+    """
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
+    """
     update_surplus_worksheet(new_surplus_data)
-
-
-
-
+    """
+    update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to love Sandwitches Data Automation")
 main()
