84cc41043447e6e85aafd3ee4dbda5d5fd2e2d09
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-07-22 19:49:37 +0000

    Add json file

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
 import gspread
-#from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2.service_account import Credentials
 from pprint import pprint
 
