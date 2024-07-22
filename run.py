e41266290e3171b3991163bca4dd725ec2746967
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-07-22 19:58:22 +0000

    Add creds file to gitignore.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-
+#add new creds file
 import gspread
 from google.oauth2.service_account import Credentials
 from pprint import pprint
@@ -9,7 +9,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('lovesandwitches-429620-4afdd438e00f.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
